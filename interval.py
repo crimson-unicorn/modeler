80c757c5befc15a3391aa41ed72f8b12046e40e7
Michael Xueyuan Han <hanx@g.harvard.edu>
2019-02-19 12:03:15 -0500

    Add more printing and info in interval.py

--- interval.py
+++ interval.py
@@ -1,13 +1,16 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--interval', required=True)
 parser.add_argument('-i', '--input', required=True)
 args = parser.parse_args()
 
 unique_sketches = set()
 sketches = list()
+total = 0
 with open(args.input, 'r') as f:
     for line in f:
+        total += 1
         sketch = line.strip().split()
         if len(sketches) == 0:
             sketches.append(frozenset(sketch))
@@ -16,4 +19,8 @@ with open(args.input, 'r') as f:
                 sketches.append(frozenset(sketch))
         unique_sketches.add(frozenset(sketch))
 
+print "File name : " + str(args.input)
+print "Interval: " + str(args.interval)
+print "Total # of Sketches: " + str(total)
 print "# of Unique Sketches: " + len(unique_sketches) + " ; # of Sketches: " + len(sketches)
+print "% of Unique Sketches: " + str(total / len(unique_sketches))
