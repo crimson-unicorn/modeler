548ad82dfc840480e8d57132911e181b1ce8285b
Michael Xueyuan Han <hanx@g.harvard.edu>
2019-04-14 17:09:04 -0400

    Fix a minor typo in both model.py and model_ot.py, 1,1 should be 1.1. No other changes.

--- model.py
+++ model.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
 
 	num_stds = args['num_stds']
 	if num_stds is None:	# If this argument is not supplied by the user, we try all possible configurations.
-		num_stds_config = [1.0, 1,1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0]
+		num_stds_config = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0]
 	else:
 		num_stds_config = [num_stds]
 
